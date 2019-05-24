563651e73f7f8b28e578748cdb082628745608e6
coco-super <sunfeiyu_java@163.com>
2019-05-24 11:38:57 +0800

    automatic_compression

---
+++ metadata.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "name": "helloworld",                                 //  ① 模板项目名称
+  "description": "Print hello world",                     //② 模板项目描述
+  "userPrompt": [                                         //③ 提示输入变量（显式变量）
+    {
+        "type": "input",                               //④ 提示输入类型
+        "name": "serviceName",                                 //⑤ 模板变量名
+        "message": "Please input serviceName?",                //⑥ 提示信息
+        "default": "{{ projectName }}"                 //⑦ 模板变量默认值
+      
+    },
+    {
+      "type": "input",                               //④ 提示输入类型
+      "name": "bucketName",                                 //⑤ 模板变量名
+      "message": "Please input bucketName?",                //⑥ 提示信息
+      "default": "{{ projectName }}"                 //⑦ 模板变量默认值
+    }
+  ],
+  "vars": {                                               //⑧ 隐式变量
+      "baz": "{{ projectName }}"
+  },
+  "copyOnlyPaths": [ "test" ]                          //⑨ 设置某些目录和文件不需要渲染，只是简单的拷贝 
+}
