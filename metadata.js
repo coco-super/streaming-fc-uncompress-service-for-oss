f10072eef275596fc0cd32e3c0eba8561ae22a55
coco-super <sunfeiyu_java@163.com>
2019-05-24 13:43:16 +0800

    automatic_compression

--- metadata.js
+++ metadata.js
@@ -1,19 +1,22 @@
 module.exports = {
-  "name": "helloworld",                                 //  ① 模板项目名称
-  "description": "Print hello world",                     //② 模板项目描述
-  "userPrompt": [                                         //③ 提示输入变量（显式变量）
+  "name": "helloworld",                                 
+  "description": "Print hello world",                    
+  "userPrompt": [                                         
     {
-        "type": "input",                               //④ 提示输入类型
-        "name": "service",                                 //⑤ 模板变量名
-        "message": "Please input serviceName?",                //⑥ 提示信息
-        "default": "{{ projectName }}"                 //⑦ 模板变量默认值
+        "type": "input",                              
+        "name": "service",                                 
+        "message": "Please input serviceName?",              
+        "default": "{{ projectName }}"                 
       
     },
     {
-      "type": "input",                               //④ 提示输入类型
-      "name": "bucketName",                                 //⑤ 模板变量名
-      "message": "Please input bucketName?",                //⑥ 提示信息
-      "default": "{{ projectName }}"                 //⑦ 模板变量默认值
+      "type": "input",                             
+      "name": "bucketName",                                 
+      "message": "Please input bucketName?",               
+      "default": "{{ projectName }}"                
     }
   ],
+  'vars': {
+    'service': '{{ projectName }}'
+  }
 }
