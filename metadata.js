e6f5be4745d7dfd93532184288c2425dde4d52f3
coco-super <sunfeiyu_java@163.com>
2019-05-24 14:46:11 +0800

    automatic_compression

--- metadata.js
+++ metadata.js
@@ -1,29 +1,33 @@
 module.exports = {
-  "name": "helloworld",                                 
-  "description": "Print hello world",                    
-  "userPrompt": [                                         
+  'name': 'helloworld',                                 
+  'description': 'Print hello world',                    
+  'userPrompt': [                                         
     {
-        "type": "input",                              
-        "name": "service",                                 
-        "message": "Please input serviceName?",              
-        "default": "{{ projectName }}"                 
+        'type': 'input',                              
+        'name': 'service',                                 
+        'message': 'Please input serviceName?',              
+        'default': '{{ projectName }}'                 
       
     },
     {
-      "type": "input",                             
-      "name": "bucketName",                                 
-      "message": "Please input bucketName?",               
-      "default": "{{ projectName }}"                
+      'type': 'input',                             
+      'name': 'bucketName',                                 
+      'message': 'Please input bucketName?',               
+      'default': '{{ projectName }}'                
     },
     {
-      "type": "input",                             
-      "name": "prefix",                                 
-      "message": "Please input prefix?"
+      'type': 'input',                             
+      'name': 'prefix',                                 
+      'message': 'Please input prefix?',
     },
     {
-      "type": "input",                             
-      "name": "suffix",                                 
-      "message": "Please input suffix?"
+      'type': 'list',                             
+      'name': 'suffix',                                 
+      'message': 'Please choose a file suffix?',
+      'choices': [
+        '.zip',
+        '.jar'
+      ]
     }
   ],
   'vars': {
